875bb908b7d7b9565fc77aa08aff920b18ec5a8e
phubui1996 <phubui1996@yahoo.com>
2023-10-04 13:40:05 -0800

    First Atempt

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
-  return 0;
+  let previousNum = 0;
+  let nextNum = 1
+  let result = 0;
+  for(let i = 1; i < num; i++){
+    result = previousNum + nextNum;
+    //console.log(` result = ${result}`);
+    previousNum = nextNum;
+    nextNum = result;
+    //console.log(`Previous: ${previousNum}, Next: ${nextNum}`);
+  }
+  return result;
 }
 
 module.exports = fibonacci;
+
+console.log(fibonacci(3)); // 2
+console.log(fibonacci(7)); // 13
